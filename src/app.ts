23b8a2ca7c436d4a498d2dedf4be7cb7867cdd3d
Paulo <paulocorreapc93@gmail.com>
2023-08-29 13:48:59 -0400

    inicializando express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Express + TS!!!");
+// console.log("Express + TS!!!");
+
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.send("Hello Express!")
+})
+
+app.listen(3000, () => {
+    console.log("Aprlicação TS + Express funcionando!")
+})
