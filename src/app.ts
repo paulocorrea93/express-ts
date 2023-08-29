defd8431fcd952474b40bd492b612bfeb60a2e9e
Paulo <paulocorreapc93@gmail.com>
2023-08-29 14:42:37 -0400

    criação de rota com POST

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,15 @@ import express from "express";
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     return res.send("Hello Express!")
+})
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+    return res.send("Produto adicionado.")
 })
 
 app.listen(3000, () => {
